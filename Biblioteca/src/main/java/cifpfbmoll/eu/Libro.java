44e2897fe79d855856ae1841a83aa2c07c94eb1a
franco <fperi90@cifpfbmoll.eu>
2021-02-17 21:15:31 +0100

    instanciado libro
    a partir de aqui los constructores

--- Biblioteca/src/main/java/cifpfbmoll/eu/Libro.java
+++ Biblioteca/src/main/java/cifpfbmoll/eu/Libro.java
@@ -1,5 +1,14 @@
 package cifpfbmoll.eu;
 
 public class Libro {
-
+	private String ISBN;
+	private String titulo;
+	private String autor;
+	private String editorial;
+	private int numero_copias;
+	private int num_copias_disponibles;
+	
+	
+	
+	
 }
